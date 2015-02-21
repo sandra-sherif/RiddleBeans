4f8e503e49f0f5509d08d87756a90d589057117f
Amr Nafie <amr.nafie@gmail.com>
2015-02-21 21:31:41 +0200

    Added admin column

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219124204) do
+ActiveRecord::Schema.define(version: 20150221145544) do
 
   create_table "blogposts", force: true do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150219124204) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
